5922e8d7bd5f6a07438965023ef1240ff76e6b79
Yashika Bansal <yashikabansal1507@gmail.com>
2026-08-07 11:13:23 +0530

    test cases

--- test.js
+++ test.js
@@ -9,3 +9,28 @@ else{
     console.log("T C-01 :index.html exists :FAIL");
     passed =false;
 }
+
+if(fs.existsSync("script.js")){
+     console.log("T C-02 :scirpt.js exists :PASS");
+}
+else{
+    console.log("T C-02 :script.js exists :FAIL");
+    passed =false;
+}
+if(fs.existsSync("studentjson")){
+     console.log("T C-03 :studentjson exists :PASS");
+}
+else{
+    console.log("T C-03 :studentjson exists :FAIL");
+    passed =false;
+}
+const students =JSON.parse(fs.readFileSyntnc("student.json"));
+const student=studnents[0];
+
+if(fs.existsSync("style.css")){
+     console.log("T C-04 :style.css exists :PASS");
+}
+else{
+    console.log("T C-04 :style.css exists :FAIL");
+    passed =false;
+}
