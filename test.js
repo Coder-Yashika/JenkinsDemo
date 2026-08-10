7cecec05db956892f549c013a22e83036e722502
Yashika Bansal <yashikabansal1507@gmail.com>
2026-08-10 10:06:12 +0530

    test cases updated

--- test.js
+++ test.js
@@ -34,3 +34,54 @@ else{
     console.log("T C-04 :style.css exists :FAIL");
     passed =false;
 }
+
+
+if(student.name.trim()!==""){
+     console.log("T C-05 :Name Validation :PASS");
+}
+else{
+    console.log("T C-05 :Name Validation :FAIL");
+    passed =false;
+}
+
+if(student.email.includes("@")){
+     console.log("T C-06 :Email Validation :PASS");
+}
+else{
+    console.log("T C-06 :Email Validation :FAIL");
+    passed =false;
+}
+
+if(student.mobile.length===10){
+     console.log("T C-07 :Mobile Validation :PASS");
+}
+else{
+    console.log("T C-07 :Mobile Validation :FAIL");
+    passed =false;
+}
+
+if(student.branch!==""){
+     console.log("T C-08 :Branch Validation :PASS");
+}
+else{
+    console.log("T C-08 :Branch Validation :FAIL");
+    passed =false;
+}
+
+if(student.password.length>=6){
+     console.log("T C-09 :Password Validation :PASS");
+}
+else{
+    console.log("T C-09 :Password Validation :FAIL");
+    passed =false;
+}
+
+if(passed){
+     console.log("T C-10 :Registration Sccessfull :PASS");
+      console.log("\nBuild SUCCESS");
+      process.exit(0);
+}
+else{
+    console.log("T C-09 :Password Validation :FAIL");
+    passed =false;
+}
